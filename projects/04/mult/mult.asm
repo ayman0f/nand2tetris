388228682703c1c1719729e351f9f3768657fd3c
Ayman <35163891+ayman0f@users.noreply.github.com>
2020-04-24 23:36:02 -0700

    Implemented mult.asm and Fill.asm

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,30 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Higher level explanation:
+// R2=0
+// while(R1 != 0)
+//     R2=R2+R0
+//     R1--
+
+@R2
+M=0	// R2=0
+(LOOP)
+    @R1
+    D=M		// D=R0
+    @END
+    D;JEQ	// if(R1==0) goto END
+
+    @R0
+    D=M		// D=R0
+    @R2
+    M=M+D	// R2=R2+R0
+
+    @R1
+    M=M-1	// R1--
+
+    @LOOP
+    0;JMP	// Goto LOOP
+(END)
+    @END
+    0;JMP	// Infinite loop
